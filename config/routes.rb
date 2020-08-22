78587989836542b44b6994194bc8711457427d48
Martin Lillebo <martin@pqa.no>
2020-08-22 13:26:38 +0200

    added correct controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "cocktails", to: "cocktails#index"
 end
+
+
+# index, new, create, show, edit, update, delete
