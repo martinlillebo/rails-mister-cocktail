4264a30795ce4ad5c2ccee4f39e86e49bde04db5
Martin Lillebo <martin@pqa.no>
2020-08-25 19:42:20 +0200

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: "cocktails#index"
   get 'doses/new'
   get 'doses/create'
   get 'doses/destroy'
   # get 'cocktails/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create]   
+  resources :cocktails, only: [:show, :new, :create]   
   resources :doses, only: [:new, :create, :destroy]
 end
 
